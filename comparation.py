f4141aaecb10cb85bf9f620c3598454c6d0646f1
Gorka Gacia <114485898+garciagorkauni@users.noreply.github.com>
2024-11-20 19:18:50 +0100

    Create main comparation program

---
+++ comparation.py
@@ -0,0 +1,8 @@
+from gpt.gpt_chatbot import gpt_response
+from llama.llama_chatbot import llama_response
+from gemini.gemini_chatbot import gemini_response
+
+
+# print('Llama: ' + llama_response(input("Para llama: ")))
+# print('Gemini: ' + gemini_response(input("Para gemini: ")))
+# print('GPT: ' + gpt_response(input("Para GPT: ")))
