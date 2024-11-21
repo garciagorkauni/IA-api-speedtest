31c1acee8b28185b08cd6f08241ff31626e28b5b
Gorka Gacia <114485898+garciagorkauni@users.noreply.github.com>
2024-11-21 21:28:45 +0100

    Develop comparation program

--- comparation.py
+++ comparation.py
@@ -1,8 +1,46 @@
+import time
+import warnings
+
 from gpt.gpt_chatbot import gpt_response
 from llama.llama_chatbot import llama_response
 from gemini.gemini_chatbot import gemini_response
 
+# Get user prompt
+prompt = input("Prompt for the chatbots: ")
 
-# print('Llama: ' + llama_response(input("Para llama: ")))
-# print('Gemini: ' + gemini_response(input("Para gemini: ")))
-# print('GPT: ' + gpt_response(input("Para GPT: ")))
+# Calculate the needed time of response of each chatbot
+# LLAMA
+try:
+    start_time = time.time()
+    llama_response(prompt)
+    end_time = time.time()
+    LLAMA_RESPONSE_TIME = end_time - start_time
+    print("Llama api response time: {}".format(LLAMA_RESPONSE_TIME))
+except:
+    print("An error ocurred getting the response of llama...")
+
+# GEMINI
+try:
+    start_time = time.time()
+    gemini_response(prompt)
+    end_time = time.time()
+    GEMINI_RESPONSE_TIME = end_time - start_time
+    print("Gemini api response time: {}".format(GEMINI_RESPONSE_TIME))
+except:
+    print("An error ocurred getting the response of gemini...")
+
+# GPT
+try:
+    start_time = time.time()
+    gpt_response(prompt)
+    end_time = time.time()
+    GPT_RESPONSE_TIME = end_time - start_time
+    print("GPT api response time: {}".format(GPT_RESPONSE_TIME))
+except:
+    print("An error ocurred getting the response of gpt...")
+
+print("\n\n")
+
+# print('Llama: ' + llama_response(prompt)
+# print('Gemini: ' + gemini_response(prompt)
+# print('GPT: ' + gpt_response(prompt)
