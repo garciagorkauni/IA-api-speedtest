db158bc6675cb795252cf8d54cd62cba1a971768
Gorka Gacia <114485898+garciagorkauni@users.noreply.github.com>
2024-11-21 21:05:00 +0100

    Fix gpt chatbot

--- gpt/gpt_chatbot.py
+++ gpt/gpt_chatbot.py
@@ -5,31 +5,13 @@ def gpt_response(prompt):
     client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
     chat_history = []
+    chat_history = [{"role": "user", "content": prompt}]
 
-    while True:
-        chat_history.append({"role": "user", "content": prompt})
+    response = client.chat.completions.create(
+        messages=chat_history,
+        # model="gpt-4o",
+        model="gpt-3.5-turbo",
+    )
 
-        response_iterator = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": "Say this is a test",
-                }
-            ],
-            # model="gpt-4o",
-            model="gpt-3.5-turbo",
-        )
-
-        collected_messages = []
-
-        for chunk in response_iterator:
-            chunk_message = chunk['choices'][0]['delta']
-            collected_messages.append(chunk_message)
-            full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
-            print(full_reply_content)
-
-            print("\033[H\033[J", end="")
-
-        chat_history.append({"role": "assistant", "content": full_reply_content})
-
-        return full_reply_content
+    full_reply_content = response['choices'][0]['message']['content']
+    return full_reply_content
